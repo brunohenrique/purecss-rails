72ed7658cdef494455bb1d65e50d13869899a1e8
Bruno Henrique - Garu <squall.bruno@gmail.com>
2013-06-02 19:03:04 -0300

    Add support for 'non-responsive'

--- lib/purecss-rails/generators/install_generator.rb
+++ lib/purecss-rails/generators/install_generator.rb
@@ -4,15 +4,27 @@ module Purecss
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates/", __FILE__)
+      argument :stylesheets_type, :type => :string, :default => 'responsive', :banner => '*responsive or nonresponsive'
+
 
       def add_purecss
+        copy_file layout_type(stylesheets_type), "app/assets/stylesheets/purecss.css"
+
         css_manifest = 'app/assets/stylesheets/application.css'
         if File.exist?(css_manifest)
-          copy_file "purecss.css", "app/assets/stylesheets/purecss.css"
           style_require_block = " *= require purecss\n"
           insert_into_file css_manifest, style_require_block, :after => "require_self\n"
         else
           copy_file "application.css", "app/assets/stylesheets/application.css"
+        end
+      end
+
+      private
+      def layout_type(type)
+        if type == 'responsive' or type == 'nonresponsive'
+          "#{stylesheets_type}.css"
+        else
+          raise "'#{stylesheets_type}'' is not recognized, use either 'responsive' or 'nonresponsive'"
         end
       end
     end
